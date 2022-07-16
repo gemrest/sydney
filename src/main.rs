5149031feba6702335c81da0e61bddc63766d65c
Fuwn <contact@fuwn.me>
2022-07-16 03:47:07 -0700

    feat: initial release

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+// This file is part of Germ <https://github.com/gemrest/sydney>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+#![deny(
+  warnings,
+  nonstandard_style,
+  unused,
+  future_incompatible,
+  rust_2018_idioms,
+  unsafe_code,
+  clippy::all,
+  clippy::nursery,
+  clippy::pedantic
+)]
+#![recursion_limit = "128"]
+
+mod app;
+mod command;
+mod input;
+mod stateful_list;
+mod ui;
+
+use app::App;
+use crossterm::{event, execute, terminal};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let mut args = std::env::args();
+  let mut app = App::new();
+
+  if let Some(arg) = args.nth(1) {
+    match arg.as_str() {
+      "--version" | "-v" => {
+        println!("{}", env!("CARGO_PKG_VERSION"));
+
+        return Ok(());
+      }
+      "--help" | "-h" => {
+        println!(
+          r#"usage: {} [option, capsule_uri]
+Options:
+    --version, -v    show version text
+    --help, -h       show help text
+
+Sample invocations:
+    {0} gemini://gem.rest/
+    {0} --help
+
+Report bugs to https://github.com/gemrest/sydney/issues"#,
+          args
+            .next()
+            .unwrap_or_else(|| env!("CARGO_PKG_NAME").to_string())
+        );
+
+        return Ok(());
+      }
+      _ => {
+        app.url = url::Url::parse(&arg)?;
+
+        app.make_request();
+      }
+    }
+  }
+
+  terminal::enable_raw_mode()?;
+
+  let mut stdout = std::io::stdout();
+
+  execute!(
+    stdout,
+    terminal::EnterAlternateScreen,
+    event::EnableMouseCapture
+  )?;
+
+  let mut terminal =
+    tui::Terminal::new(tui::backend::CrosstermBackend::new(stdout))?;
+  let result =
+    App::run(&mut terminal, app, std::time::Duration::from_millis(250));
+
+  terminal::disable_raw_mode()?;
+  execute!(
+    terminal.backend_mut(),
+    terminal::LeaveAlternateScreen,
+    event::DisableMouseCapture
+  )?;
+  terminal.show_cursor()?;
+
+  if let Err(err) = result {
+    println!("{:?}", err);
+  }
+
+  Ok(())
+}
---
+++ src/stateful_list.rs
@@ -0,0 +1,82 @@
+// This file is part of Germ <https://github.com/gemrest/sydney>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+//! <https://github.com/fdehau/tui-rs/blob/master/examples/list.rs>
+
+use tui::widgets::ListState;
+
+pub struct StatefulList<T> {
+  pub state:    ListState,
+  pub items:    Vec<T>,
+  pub selected: usize,
+}
+
+impl<T> StatefulList<T> {
+  pub fn with_items(items: Vec<T>) -> Self {
+    Self {
+      state: ListState::default(),
+      items,
+      selected: 0,
+    }
+  }
+
+  pub fn next(&mut self) {
+    let i = match self.state.selected() {
+      Some(i) =>
+        if i >= self.items.len() - 1 {
+          0
+        } else {
+          i + 1
+        },
+      None => 0,
+    };
+
+    self.selected = i;
+
+    self.state.select(Some(i));
+  }
+
+  pub fn previous(&mut self) {
+    let i = match self.state.selected() {
+      Some(i) =>
+        if i == 0 {
+          self.items.len() - 1
+        } else {
+          i - 1
+        },
+      None => 0,
+    };
+
+    self.selected = i;
+
+    self.state.select(Some(i));
+  }
+
+  pub fn last(&mut self) {
+    self.state.select(Some(self.items.len() - 1));
+
+    self.selected = self.items.len() - 1;
+  }
+
+  pub fn first(&mut self) {
+    self.state.select(Some(0));
+
+    self.selected = 0;
+  }
+
+  pub fn unselect(&mut self) { self.state.select(None); }
+}
