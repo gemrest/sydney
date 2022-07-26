fa92fdfac1a40d2b72bd1eea59d27be216284904
Fuwn <contact@fuwn.me>
2022-07-26 17:48:38 +0000

    feat(src): prefix urls everywhere

---
+++ src/url.rs
@@ -0,0 +1,24 @@
+// This file is part of Sydney <https://github.com/gemrest/sydney>.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+pub fn prefix_gemini(url: &str) -> String {
+  if url.starts_with("gemini://") {
+    url.to_string()
+  } else {
+    format!("gemini://{}", url)
+  }
+}
